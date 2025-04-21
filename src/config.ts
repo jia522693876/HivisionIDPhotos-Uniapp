d13ff5baa83e208e86c88a2f94d0b8b1e902d752
jia zhaoyang <522693876@qq.com>
2025-04-21 23:08:44 +0800

    部署ssl

--- src/config.ts
+++ src/config.ts
@@ -5,22 +5,22 @@ interface EnvironmentConfig {
 const baseApi: { [key: string]: EnvironmentConfig } = {
   // 开发版
   development: {
-     host: "http://lianpujt.com:9191",
+     host: "https://lianpujt.com:9191",
 	 //host: "http://192.168.2.184:8080",
   },
   // 体验版
   trial: {
-     host: "http://lianpujt.com:9191",
+     host: "https://lianpujt.com:9191",
 	 //host: "http://192.168.2.184:8080",
   },
   // 正式版
   release: {
-     host: "http://lianpujt.com:9191",
+     host: "https://lianpujt.com:9191",
 	 //host: "http://192.168.2.184:8080",
   },
   // uniapp 正式环境
   production: {
-    host: "http://lianpujt.com:9191",
+    host: "https://lianpujt.com:9191",
 	//host: "http://192.168.2.184:8080",
 	
   },
